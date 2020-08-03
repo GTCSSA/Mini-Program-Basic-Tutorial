f60824e780e7020c383907ed5e3a77e119761b58
Alice Gao <ruiyi.gao@gatech.edu>
2020-08-03 15:11:55 -0400

    basic_component_update

---
+++ pages/3.3_ShuJuBangDing/3.3.js
@@ -0,0 +1,73 @@
+// pages/3.3/3.3.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    title: "CSSA小程序教程",  //这是一个文字
+    listArr: ["技术部", "宣传部", "内联部"],  //这是一个array
+    //这是一个object
+    obj: { 
+      name: "技术部",
+      maxMember: 15,  //这是一个数字
+      full: false,  //这是一个boolean
+    }
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
