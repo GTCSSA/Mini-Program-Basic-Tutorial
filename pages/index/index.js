54507730ea86bc3a311728caadc89f8b17f320ef
Alice Gao <ruiyi.gao@gatech.edu>
2020-08-08 20:42:09 -0400

    Final Project

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,95 @@
-//index.js
-//获取应用实例
-const app = getApp()
+import common from "../../utils/common.js";
 
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    dataList:[]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getRequest();
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+
+  getRequest(){
+    //发送网络请求
+    wx.request({
+      url: 'https://ku.qingnian8.com/school/list.php',
+      data:{
+        num: 5,
+      },
+      success:res=>{
+        res.data.forEach(item =>{
+          // var time = item.posttime*1000; //time是一个时间戳
+          // var d = new Date(time);
+          // var year = d.getFullYear();
+          // //月份要加一 因为默认是0-11月
+          // //这里用到了ternary operation 格式为 condition ? (if true, then ...) : (if false, then ...)
+          // var month = (d.getMonth() + 1) < 10 ? "0" + (d.getMonth() + 1) : (d.getMonth() + 1); 
+          // var day = d.getDate() < 10 ? "0" + (d.getDate()) : (d.getDate());
+          // var posttime = year + "-" + month + "-" + day
+          // item.posttime = posttime
+          item.posttime = common.getMyDate(item.posttime, "Y-m-d")
+          item.title=common.getStrLen(item.title, 25)
+        })
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          dataList: res.data
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
 })
